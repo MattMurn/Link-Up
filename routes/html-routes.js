bcdfe7582c3f0937445f9a70dc2d2340f4e5d953
Brian Parkes <parkesbrianj@gmail.com>
2018-03-17 11:41:48 -0500

    handlebar partials functioning, choices data successfully making it's way to the browser

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -14,7 +14,25 @@ module.exports = function(app) {
 
   // index route loads view.html
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.render("index");
+    // res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
+    // Page for entering data for a new person
+    //      Only this single route is needed for all of the input categories
+    //      To display the correct template the question object must be used with handlebars
+    //      
+    app.get("/addnew", function(req, res) {
+      var question = {
+        radio: false,
+        oneClick: false,
+        userEntry: true,
+        options: ["one", "two", "three"]
+      }
+      var questionObject = {
+        question: question
+      }
+
+      res.render("question", questionObject);
+    });
 };
