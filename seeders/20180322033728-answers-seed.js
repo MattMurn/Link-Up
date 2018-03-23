1f6c2a23fb51331860971e21e3b127595832e7e0
Matthew Murnighan <mattmurn@gmail.com>
2018-03-22 19:35:58 -0500

    changed types on answer-seed

--- seeders/20180322033728-answers-seed.js
+++ seeders/20180322033728-answers-seed.js
@@ -4,6 +4,7 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
       return queryInterface.bulkInsert('answers', [
         {
+            type: 'textShort',
             text: '', 
             contactCol: 'name',
             questionId: 1,
@@ -12,7 +13,7 @@ module.exports = {
         },
 
         {
-
+            type: 'textShort',
             text: '', 
             contactCol: 'location',
             questionId: 2,
@@ -21,7 +22,161 @@ module.exports = {
 
         },
         {
-            type: '',
+            type: 'selectOne',
+            text: 'Professional', 
+            contactCol: 'category',
+            questionId: 3,
+            createdAt: new Date(),
+            updatedAt: new Date()
+
+        },
+        {
+            type: 'selectOne',
+            text: 'Personal', 
+            contactCol: 'category',
+            questionId: 3,
+            createdAt: new Date(),
+            updatedAt: new Date()
+
+        },
+        {
+            type: 'textShort',
+            text: '', 
+            contactCol: 'profession',
+            questionId: 4,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        },
+        {
+            type: 'selectOne',
+            text: 'Male', 
+            contactCol: 'gender',
+            questionId: 5,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        },
+        {
+            type: 'select',
+            text: 'Female', 
+            contactCol: 'gender',
+            questionId: 5,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        },
+        {
+            type: 'textShort',
+            text: '', 
+            contactCol: 'gender',
+            questionId: 5,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        },
+        {
+            type: 'selectOne',
+            text: '0-18', 
+            contactCol: 'age',
+            questionId: 6,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        },
+        {
+            type: 'selectOne',
+            text: '18-39', 
+            contactCol: 'age',
+            questionId: 6,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        },
+        {
+            type: 'selectOne',
+            text: '40-65', 
+            contactCol: 'age',
+            questionId: 6,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        },
+        {
+            type: 'selectOne',
+            text: '65+', 
+            contactCol: 'age',
+            questionId: 6,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        },
+        {
+            type: 'selectOne',
+            text: 'light', 
+            contactCol: 'complexion',
+            questionId: 7,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        },
+        {
+            type: 'selectOne',
+            text: 'medium-light', 
+            contactCol: 'complexion',
+            questionId: 7,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        },
+        {
+            type: 'selectOne',
+            text: 'meduim', 
+            contactCol: 'complexion',
+            questionId: 7,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        },
+        {
+            type: 'selectOne',
+            text: 'meduim-dark', 
+            contactCol: 'complexion',
+            questionId: 7,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        },
+        {
+            type: 'selectOne',
+            text: 'dark', 
+            contactCol: 'complexion',
+            questionId: 7,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        },
+        {
+            type: 'selectOne',
+            text: 'skinny', 
+            contactCol: 'build',
+            questionId: 8,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        },
+        {
+            type: 'selectOne',
+            text: 'low key thicc', 
+            contactCol: 'build',
+            questionId: 8,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        },
+        {
+            type: 'selectOne',
+            text: 'high key thicc', 
+            contactCol: 'build',
+            questionId: 8,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        },
+        {
+            type: 'selectOne',
+            text: 'LARGE', 
+            contactCol: 'build',
+            questionId: 8,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        },
+        {
+            type: 'selectOne',
             text: 'tall', 
             contactCol: 'height',
             questionId: 9,
@@ -29,7 +184,7 @@ module.exports = {
             updatedAt: new Date()
         },
         {
-            type: '',
+            type: 'selectOne',
             text: 'medium', 
             contactCol: 'height',
             questionId: 9,
@@ -37,7 +192,7 @@ module.exports = {
             updatedAt: new Date()
         },
         {
-            type: '',
+            type: 'selectOne',
             text: 'small', 
             contactCol: 'height',
             questionId: 9,
@@ -45,24 +200,56 @@ module.exports = {
             updatedAt: new Date()
         },
         {
-            type: '',
-            text: 'tall', 
-            contactCol: 'profession',
-            questionId: 4,
+            type: 'selectOne',
+            text: 'black', 
+            contactCol: 'hair',
+            questionId: 10,
             createdAt: new Date(),
             updatedAt: new Date()
         },
         {
-            type: '',
-            text: 'tall', 
+            type: 'selectOne',
+            text: 'brown', 
+            contactCol: 'hair',
+            questionId: 10,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        },
+        {
+            type: 'selectOne',
+            text: 'gray', 
+            contactCol: 'hair',
+            questionId: 10,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        },
+        {
+            type: 'selectOne',
+            text: 'blonde', 
+            contactCol: 'hair',
+            questionId: 10,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        },
+        {
+            type: 'selectOne',
+            text: 'white', 
+            contactCol: 'hair',
+            questionId: 10,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        },
+        {
+            type: 'text',
+            text: '', 
             contactCol: 'clothing',
             questionId: 11,
             createdAt: new Date(),
             updatedAt: new Date()
         },
         {
-            type: '',
-            text: 'tall', 
+            type: 'text',
+            text: '', 
             contactCol: 'notes',
             questionId: 1,
             createdAt: new Date(),
