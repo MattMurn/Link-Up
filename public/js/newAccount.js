d9587c5f05e7a82bb7878bdb48e7824f7a2ed760
Matthew Murnighan <mattmurn@gmail.com>
2018-03-23 15:32:51 -0500

    working newUser page that hits post route and adds new user to db password is showing

--- public/js/newAccount.js
+++ public/js/newAccount.js
@@ -1,15 +1,18 @@
 //add this page to handlebars
 //make click events. 
 $(document).ready(function() {
-    $('.newSubmit').on('click', function(){
+    
+    $('#newSubmit').on('click', function(){
+        event.preventDefault();
         var newUser = {
-            firstName: $().val().trim(),
-            lastName: $().val().trim(),
-            email: $().val().trim(),
-            password: $().val().trim()
+            firstName: $('#firstName').val().trim(),
+            lastName: $('#lastName').val().trim(),
+            email: $('#email').val().trim(),
+            password: $('#password').val().trim()
         };
-
-        console.log("newSubmit linked");    
+        console.log(newUser);
+        $.post("/api/user", newUser)
+        .then(window.location.href = "/");
 
     });
 
