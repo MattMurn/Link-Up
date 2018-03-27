45dfc781413413e288e8709a8dd9deef1665d4cf
Matthew Murnighan <mattmurn@gmail.com>
2018-03-27 14:37:32 -0500

    controller update

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -2,16 +2,10 @@ var db = require('../models');
 var passport = require('passport');
 module.exports = function (app) {
 
-// app.get("/login", function(req, res){
-//     res.render("login");
-//     console.log("linked");
-//   });
-
-
 app.post('/login', 
   passport.authenticate('local', {successRedirect: '/homepage', failureRedirect: '/signup' }),
   function(req, res) {
-    console.log('route hit');
+    // console.log('route hit');
 
     // res.render('index');
   });
